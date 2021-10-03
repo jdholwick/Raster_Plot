4b8b8ab77dfa8076533224f4bd4526643d8bd2d0
J.D. Holwick <70964794+jdholwick@users.noreply.github.com>
2021-10-03 11:30:02 -0600

    minor adjustments:   - none noted

--- main_viz.js
+++ main_viz.js
@@ -11,8 +11,8 @@
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
     d3.queue()
-        .defer(d3.json, "data/world.topojson") // first item here so second in 'function ready()'
-        .defer(d3.csv, "data/city-data.csv") // second item here so third in 'function ready()'
+        .defer(d3.json, "/data/world.topojson") // first item here so second in 'function ready()'
+        .defer(d3.csv, "/data/city-data.csv") // second item here so third in 'function ready()'
         .await(ready)
 
     // whenever there are shapes on a map we want to use 'geoMercator' and 'geoPath' apparently
